04e2a9a8d342939759d377da354369fd479e13f0
Jason Brown and Matt Kelley <jason+matt@>
2017-03-06 15:59:17 -0800

    add Tag class and passing tests, update the db with tag_id datatype

---
+++ src/Tag.php
@@ -0,0 +1,75 @@
+<?php
+    Class Tag
+    {
+        private $tag;
+        private $id;
+
+        function __construct($tag, $id = null)
+        {
+            $this->tag = $tag;
+            $this->id = $id;
+        }
+
+        function getTag()
+        {
+            return $this->tag;
+        }
+
+        function setTag($tag)
+        {
+            $this->tag = $tag;
+        }
+
+        function getId()
+        {
+            return $this->id;
+        }
+
+        function save()
+        {
+            $GLOBALS['DB']->exec("INSERT INTO tags (tag) VALUES ('{$this->getTag()}');");
+            $this->id = $GLOBALS['DB']->lastInsertId();
+        }
+
+        static function getAll()
+        {
+            $returned_tags = $GLOBALS['DB']->query("SELECT * FROM tags;");
+            $tags = array();
+            foreach($returned_tags as $tag) {
+                $tag_text = $tag['tag'];
+                $tag_id = $tag['tag_id'];
+                $new_tag = new Tag($tag_text, $tag_id);
+                array_push($tags, $new_tag);
+            }
+            return $tags;
+        }
+
+        static function deleteAll()
+        {
+          $GLOBALS['DB']->exec("DELETE FROM tags;");
+          $GLOBALS['DB']->exec("DELETE FROM comments_tags");
+          $GLOBALS['DB']->exec("DELETE FROM threads_tags");
+        }
+
+        static function find($search_id)
+        {
+            $found_tag = null;
+            $tags = Tag::getAll();
+            foreach($tags as $tag) {
+                $tag_id = $tag->getId();
+                if ($tag_id == $search_id) {
+                  $found_tag = $tag;
+                }
+            }
+            return $found_tag;
+        }
+
+        function delete()
+        {
+            $GLOBALS['DB']->exec("DELETE FROM tags WHERE tag_id = {$this->getId()};");
+            $GLOBALS['DB']->exec("DELETE FROM threads_tags WHERE tag_id = {$this->getId()};");
+            $GLOBALS['DB']->exec("DELETE FROM comments_tags WHERE tag_id = {$this->getId()};");
+        }
+    }
+
+?>
---
+++ tests/TagTest.php
@@ -0,0 +1,177 @@
+<?php
+
+    /**
+    * @backupGlobals disabled
+    * @backupStaticAttributes disabled
+    */
+
+    require_once "src/Comment.php";
+    require_once "src/User.php";
+    require_once "src/Tag.php";
+    require_once "src/Thread.php";
+
+    $server = 'mysql:host=localhost:8889;dbname=forum_test';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+
+    class TagTest extends PHPUnit_Framework_TestCase
+    {
+
+        protected function tearDown()
+        {
+            Tag::deleteAll();
+        }
+
+        function testGetTag()
+        {
+            //Arrange
+            $tag = "dogs";
+            $id = 1;
+
+            $testGetTag = new Tag($tag, $id);
+
+            //Act
+            $result = $testGetTag->getId();
+
+            //Assert
+            $this->assertEquals($id, $result);
+
+        }
+
+        function testSetTag()
+        {
+            //Arrange
+            $tag = "dogs";
+            $id = 1;
+
+            $testSetTag = new Tag($tag, $id);
+
+            //Act
+            $testSetTag->setTag("cats");
+            $result = $testSetTag->getTag();
+
+            //Assert
+            $this->assertEquals("cats", $result);
+        }
+
+        function testGetId()
+        {
+            //Arrange
+            $tag = "dogs";
+            $id = 1;
+
+            $testGetId = new Tag($tag, $id);
+
+            //Act
+            $result = $testGetId->getId();
+
+            //Assert
+            $this->assertEquals($id, $result);
+        }
+
+        function testSave()
+        {
+            //Arrange
+            $tag = "dogs";
+
+            $testSave = new Tag($tag);
+            $testSave->save();
+
+            //Act
+            $result = Tag::getAll();
+
+            //Assert
+            $this->assertEquals($testSave, $result[0]);
+        }
+
+        function testDeleteTag()
+        {
+            //Arrange
+            $tag = "dogs";
+
+            $testSave = new Tag($tag);
+            $testSave->save();
+
+            $tag2 = "cats";
+
+            $testSave2 = new Tag($tag2);
+            $testSave2->save();
+
+            //Act
+            $testSave2->delete();
+
+            //Assert
+            $this->assertEquals([$testSave], Tag::getAll());
+        }
+
+        function testGetAll()
+        {
+            //Arrange
+            $tag = "dogs";
+            $id = 1;
+
+            $testSave = new Tag($tag, $id);
+            $testSave->save();
+
+            $tag2 = "cats";
+            $id2 = 2;
+
+            $testSave2 = new Tag($tag, $id);
+            $testSave2->save();
+
+            //Act
+            $result = Tag::getAll();
+
+            //Assert
+            $this->assertEquals([$testSave, $testSave2], $result);
+        }
+
+        function testDeleteAll()
+        {
+            //Arrange
+            $tag = "dogs";
+            $id = 1;
+
+            $testSave = new Tag($tag, $id);
+            $testSave->save();
+
+            $tag2 = "cats";
+            $id2 = 2;
+
+            $testSave2 = new Tag($tag, $id);
+            $testSave2->save();
+
+            //Act
+            Tag::deleteAll();
+
+            //Assert
+            $result = Tag::getAll();
+            $this->assertEquals([], $result);
+        }
+
+        function testFind()
+        {
+            //Arrange
+            $tag = "dogs";
+            $id = 1;
+
+            $testSave = new Tag($tag, $id);
+            $testSave->save();
+
+            $tag2 = "cats";
+            $id2 = 2;
+
+            $testSave2 = new Tag($tag, $id);
+            $testSave2->save();
+
+            //Act
+            $result = Tag::find($testSave->getId());
+
+            //Assert
+            $this->assertEquals($testSave, $result);
+        }
+    }
+
+?>
