2dfdbb67361d3e10a5200a0fdc3d59070d9b6163
Jason Brown and Matt Kelley <matt@>
2017-03-08 12:09:30 -0800

    pass update tests

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -38,17 +38,17 @@
             $country = "USA";
             $score = 372193;
             $create = "2017-01-01 12:00:00";
-        
+
             $testGetUserName = new User($username, $password, $user_photo, $rank, $bio, $city, $state, $country, $score, $create);
-        
+
             //Act
             $result = $testGetUserName->getUserName();
-        
+
             //Assert
             $this->assertEquals($username, $result);
-        
+
         }
-        
+
         function testSetUserName()
         {
             //Arrange
@@ -62,13 +62,13 @@
             $country = "USA";
             $score = 372193;
             $create = "2017-01-01 12:00:00";
-        
+
             $testGetUserName = new User($username, $password, $user_photo, $rank, $bio, $city, $state, $country, $score, $create);
-        
+
             //Act
             $testGetUserName->setUsername("Bob");
             $result = $testGetUserName->getUserName();
-        
+
             //Assert
             $this->assertEquals("Bob", $result);
         }
@@ -87,16 +87,16 @@
             $score = 372193;
             $create = "2017-01-01 12:00:00";
             $id = 1;
-        
+
             $testUser = new User($username, $password, $user_photo, $rank, $bio, $city, $state, $country, $score, $create, $id);
-        
+
             //Act
             $result = $testUser->getId();
-        
+
             //Assert
             $this->assertEquals($id, $result);
         }
-        
+
         function testSave()
         {
             //Arrange
@@ -110,17 +110,17 @@
             $country = "USA";
             $score = 372193;
             $create = "2017-01-01 12:00:00";
-        
+
             $testUser = new User($username, $password, $user_photo, $rank, $bio, $city, $state, $country, $score, $create);
             $testUser->save();
-        
+
             //Act
             $result = User::getAll();
-        
+
             //Assert
             $this->assertEquals($testUser, $result[0]);
         }
-        
+
         function testUpdate()
         {
             //Arrange
@@ -134,22 +134,49 @@
             $country = "USA";
             $score = 372193;
             $create = "2017-01-01 12:00:00";
-        
+
             $testUser = new User($username, $password, $user_photo, $rank, $bio, $city, $state, $country, $score, $create);
             $testUser->save();
-        
+
             $new_city = "Mike";
             $new_state = "Mike";
             $new_country = "Mike";
-            $new_bio = "Mike";
-        
+
+
             //Act
-            $testUser->update($new_city, $new_state, $new_country, $new_bio);
-        
+            $testUser->update($new_city, $new_state, $new_country);
+
             //Assert
             $this->assertEquals($new_city, $testUser->getLocation_city());
         }
-        
+
+        function testUpdateBio()
+        {
+            //Arrange
+            $username = "Mark";
+            $password = "pw";
+            $user_photo = "imgur.com/dasdasd";
+            $rank = "admin";
+            $bio = "I am Mark";
+            $city = "Portland";
+            $state = "OR";
+            $country = "USA";
+            $score = 372193;
+            $create = "2017-01-01 12:00:00";
+
+            $testUser = new User($username, $password, $user_photo, $rank, $bio, $city, $state, $country, $score, $create);
+            $testUser->save();
+
+
+            $new_bio = "Mike";
+
+            //Act
+            $testUser->updateBio($new_bio);
+
+            //Assert
+            $this->assertEquals($new_bio, $testUser->getBio());
+        }
+
         function testDeleteUser()
         {
             //Arrange
@@ -163,10 +190,10 @@
             $country = "USA";
             $score = 372193;
             $create = "2017-01-01 12:00:00";
-        
+
             $testUser = new User($username, $password, $user_photo, $rank, $bio, $city, $state, $country, $score, $create);
             $testUser->save();
-        
+
             $username2 = "John";
             $password2 = "pwq";
             $user_photo2 = "imgur.com/fsdfs";
@@ -177,17 +204,17 @@
             $country2 = "USA";
             $score2 = 12324;
             $create2 = "2017-02-01 12:01:00";
-        
+
             $testUser2 = new User($username2, $password2, $user_photo2, $rank2, $bio2, $city2, $state2, $country2, $score2, $create2);
             $testUser2->save();
-        
+
             //Act
             $testUser->delete();
-        
+
             //Assert
             $this->assertEquals([$testUser2], User::getAll());
         }
-        
+
         function testGetAll()
         {
             //Arrange
@@ -201,10 +228,10 @@
             $country = "USA";
             $score = 372193;
             $create = "2017-01-01 12:00:00";
-        
+
             $testUser = new User($username, $password, $user_photo, $rank, $bio, $city, $state, $country, $score, $create);
             $testUser->save();
-        
+
             $username2 = "John";
             $password2 = "pwq";
             $user_photo2 = "imgur.com/fsdfs";
@@ -215,17 +242,17 @@
             $country2 = "USA";
             $score2 = 12324;
             $create2 = "2017-02-01 12:01:00";
-        
+
             $testUser2 = new User($username2, $password2, $user_photo2, $rank2, $bio2, $city2, $state2, $country2, $score2, $create2);
             $testUser2->save();
-        
+
             //Act
             $result = User::getAll();
-        
+
             //Assert
             $this->assertEquals([$testUser, $testUser2], $result);
         }
-        
+
         function testDeleteAll()
         {
             //Arrange
@@ -239,10 +266,10 @@
             $country = "USA";
             $score = 372193;
             $create = "2017-01-01 12:00:00";
-        
+
             $testUser = new User($username, $password, $user_photo, $rank, $bio, $city, $state, $country, $score, $create);
             $testUser->save();
-        
+
             $username2 = "John";
             $password2 = "pwq";
             $user_photo2 = "imgur.com/fsdfs";
@@ -253,18 +280,18 @@
             $country2 = "USA";
             $score2 = 12324;
             $create2 = "2017-02-01 12:01:00";
-        
+
             $testUser2 = new User($username2, $password2, $user_photo2, $rank2, $bio2, $city2, $state2, $country2, $score2, $create2);
             $testUser2->save();
-        
+
             //Act
             User::deleteAll();
-        
+
             //Assert
             $result = User::getAll();
             $this->assertEquals([], $result);
         }
-        
+
         function testFind()
         {
             //Arrange
@@ -278,10 +305,10 @@
             $country = "USA";
             $score = 372193;
             $create = "2017-01-01 12:00:00";
-        
+
             $testUser = new User($username, $password, $user_photo, $rank, $bio, $city, $state, $country, $score, $create);
             $testUser->save();
-        
+
             $username2 = "John";
             $password2 = "pwq";
             $user_photo2 = "imgur.com/fsdfs";
@@ -292,17 +319,17 @@
             $country2 = "USA";
             $score2 = 12324;
             $create2 = "2017-02-01 12:01:00";
-        
+
             $testUser2 = new User($username2, $password2, $user_photo2, $rank2, $bio2, $city2, $state2, $country2, $score2, $create2);
             $testUser2->save();
-        
+
             //Act
             $result = User::find($testUser->getId());
-        
+
             //Assert
             $this->assertEquals($testUser, $result);
         }
-        
+
         function testLogin(){
           $username = "Mark";
           $password = password_hash('password', CRYPT_BLOWFISH);
@@ -317,13 +344,13 @@
 
           $testUser = new User($username, $password, $user_photo, $rank, $bio, $city, $state, $country, $score, $create);
           $testUser->save();
-          
+
           $user = User::logIn($username, 'password');
-          
+
           $this->assertEquals($user, $testUser);
-          
+
         }
-        
+
         function testLoginFail(){
           $username = "Mark";
           $password = password_hash('password', CRYPT_BLOWFISH);
@@ -338,13 +365,13 @@
 
           $testUser = new User($username, $password, $user_photo, $rank, $bio, $city, $state, $country, $score, $create);
           $testUser->save();
-          
+
           $user = User::logIn($username, 'password1');
-          
+
           $this->assertEquals($user, false);
-          
+
         }
-        
+
     }
 
 ?>
