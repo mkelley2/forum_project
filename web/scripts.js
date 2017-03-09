463f2a7503445698f5c16791e67bdea337a23d09
mkelley2 <[m_kelley2@yahoo.com]>
2017-03-08 20:32:00 -0800

    update style for comments

--- web/scripts.js
+++ web/scripts.js
@@ -24,18 +24,64 @@ $(document).ready(function(){
   function loop(){
     for(i=0;i<copy.length;i++){
       if(copy[i].parent_id == false){
-        $(".comment-holder").append("<div class='comment-parent' id='"+ copy[i].comment_id + "'><div class='row'><div class'col-sm-10'><input" +
-        "type='hidden' name='currentUrl' value='" + document.referrer + "'><p>" + "Score:" +  copy[i].score + " - " + copy[i].comment + "</p></div><div class'col-sm-2'><form class='button-form' action='/score/" + copy[i].comment_id + "' method='post'> <input type='hidden' name='_method' value='patch'><input type='hidden' name='inputScore' value='1'><button class='button-add' type='submit' name='like-button'><img src='/img/like.png'></button></form><form class='button-form' action='/score/" + copy[i].comment_id + "'method='post'> <input type='hidden' name='_method' value='patch'> <input type='hidden' name='inputScore' value='-1'><button class='button-add' type='submit'" +
-        "name='dislike-button'><img src='/img/dislike.png'></button></form><button type='button' name='reply-button' class='reply-button' value='"+ copy[i].comment_id +"'>Reply</button><div class='reply-form'></div></div> </div> </div>");
+        $(".comment-holder").append(
+          "<div class='comment-parent panel-body' id='" + copy[i].comment_id + "'>" +
+              "<div class='col-xs-1'>" +
+                "<p class='pScore'>" + copy[i].score + "</p>" +
+                "<form class='button-form' action='/score/" + copy[i].comment_id + "' method='post'>" +
+                  "<input type='hidden' name='_method' value='patch'>" +
+                  "<input type='hidden' name='inputScore' value='1'>" +
+                  "<button class='button-add' type='submit' name='like-button'><img src='/img/like.png'></button>" +
+                "</form>" +
+                "<form class='button-form' action='/score/copy[i].comment_id'method='post'>" +
+                  "<input type='hidden' name='_method' value='patch'>" +
+                  "<input type='hidden' name='inputScore' value='-1'>" +
+                  "<button class='button-add' type='submit' name='dislike-button'><img src='/img/dislike.png'></button>" +
+                "</form>" +
+              "</div>" +
+              "<div class='col-xs-10'>" +
+                "<div class='row'>" +
+                  "<p><a href='" + copy[i].user_id + "'>User</a> - " + copy[i].comment + "</p>" +
+                "</div>" +
+                "<div class='row'>" +
+                  "<button type='button' name='reply-button' class='reply-button' value='" + copy[i].comment_id + "'>Reply</button>" +
+                  "<div class='reply-form'></div>" +
+                "</div>" +
+              "</div>" +
+          "</div>"
+          );
         added.push(copy[i]);
         copy.splice(i,1);
         loop();
 
       }else{
         if(search(copy[i].parent_id,added)){
-          $("#"+copy[i].parent_id).append("<div class='comment-parent' id='"+ copy[i].comment_id + "'><div class='row'><div class'col-sm-10'><input" +
-          "type='hidden' name='currentUrl' value='" + document.referrer + "'><p>" + "Score:" +  copy[i].score + " - " + copy[i].comment + "</p></div><div class'col-sm-2'><form class='button-form' action='/score/" + copy[i].comment_id + "' method='post'> <input type='hidden' name='_method' value='patch'><input type='hidden' name='inputScore' value='1'><button class='button-add' type='submit' name='like-button'><img src='/img/like.png'></button></form><form class='button-form' action='/score/" + copy[i].comment_id + "'method='post'> <input type='hidden' name='_method' value='patch'> <input type='hidden' name='inputScore' value='-1'><button class='button-add' type='submit'" +
-          "name='dislike-button'><img src='/img/dislike.png'></button></form><button type='button' name='reply-button' class='reply-button' value='"+ copy[i].comment_id +"'>Reply</button><div class='reply-form'></div></div> </div> </div>");
+          $("#"+copy[i].parent_id).append(
+            "<div class='comment panel-body' id='" + copy[i].comment_id + "'>" +
+                "<div class='col-xs-1'>" +
+                  "<p class='pScore'>" + copy[i].score + "</p>" +
+                  "<form class='button-form' action='/score/" + copy[i].comment_id + "' method='post'>" +
+                    "<input type='hidden' name='_method' value='patch'>" +
+                    "<input type='hidden' name='inputScore' value='1'>" +
+                    "<button class='button-add' type='submit' name='like-button'><img src='/img/like.png'></button>" +
+                  "</form>" +
+                  "<form class='button-form' action='/score/copy[i].comment_id'method='post'>" +
+                    "<input type='hidden' name='_method' value='patch'>" +
+                    "<input type='hidden' name='inputScore' value='-1'>" +
+                    "<button class='button-add' type='submit' name='dislike-button'><img src='/img/dislike.png'></button>" +
+                  "</form>" +
+                "</div>" +
+                "<div class='col-xs-10'>" +
+                  "<div class='row'>" +
+                    "<p><a href='" + copy[i].user_id + "'>User</a> - " + copy[i].comment + "</p>" +
+                  "</div>" +
+                  "<div class='row'>" +
+                    "<button type='button' name='reply-button' class='reply-button' value='" + copy[i].comment_id + "'>Reply</button>" +
+                    "<div class='reply-form'></div>" +
+                  "</div>" +
+                "</div>" +
+            "</div>"
+          );
           added.push(copy[i]);
           copy.splice(i,1);
           loop();
